8a4cca40c7cd7fc3a0ba6c2f4e1571781c7e2e4e
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-10 20:53:33 +0200

    refactor

---
+++ local/modules/up.cake/install/lang/en/install/index.php
@@ -0,0 +1,6 @@
+<?php
+$MESS ['UP_TASKS_MODULE_NAME'] = "CakeMe";
+$MESS ['UP_TASKS_MODULE_DESCRIPTION'] = "Yet another task management tool";
+$MESS ['UP_TASKS_INSTALL_TITLE'] = "Install module";
+$MESS ['UP_TASKS_UNINSTALL_TITLE'] = "Delete module";
+$MESS ['UP_TASKS_DENIED'] = "Access closed";
