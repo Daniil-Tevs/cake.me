846b2e9f8c8547dc3d641cb95f11f5cbefd51c76
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-10 16:08:44 +0200

    refactor

--- local/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$MESS ['UP_CAKE_MODULE_NAME'] = "CakeMe";
-$MESS ['UP_CAKE_MODULE_DESCRIPTION'] = "Проект для управления задачами";
-$MESS ['UP_CAKE_INSTALL_TITLE'] = "Установка модуля";
-$MESS ['UP_CAKE_UNINSTALL_TITLE'] = "Удаление модуля";
-$MESS ['UP_CAKE_DENIED'] = "Доступ закрыт";
--- local/modules/up.cake/install/components/up/cake.list/class.php
+++ local/modules/up.cake/install/components/up/cake.list/class.php
@@ -1,24 +1,20 @@
 <?php
 
-class TaskListComponent extends CBitrixComponent
+class CakeListComponent extends CBitrixComponent
 {
 	public function executeComponent()
 	{
-		$this->fetchTasks($this->arParams['title']);
+		$this->fetchRecepies();
 		$this->includeComponentTemplate();
 	}
 
-	public function onPrepareComponentParams($arParams)
+	protected function fetchRecepies()
 	{
-		$arParams['title'] = (trim(request()->get('title')) !== '') ? trim(request()->get('title')) : null;
-		$arParams['DATE_FORMAT'] = $arParams['DATE_FORMAT'] ?? 'H:i d-m-Y';
-
-		return $arParams;
-	}
-
-
-	protected function fetchTasks($title)
-	{
-		$this->arResult['TASKS'] = \UP\Tasks\API\TaskList::get($title);
+		$this->arResult['RECEPIES'] = [
+			'1' => [
+				'NAME' => 'name',
+				'DESCRIPTION' => 'asdasda',
+			]
+		];
 	}
 }
