6bb7aa6b1f855af314cbdcdbcbb9c7bb4c824f5c
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-14 17:22:04 +0200

    RecipeService.php: some refactor

--- local/modules/up.cake/lib/Service/RecipeService.php
+++ local/modules/up.cake/lib/Service/RecipeService.php
@@ -2,9 +2,6 @@
 
 namespace Up\Cake\Service;
 
-use Bitrix\Main\Entity\Query;
-use http\QueryString;
-use UP\Cake\Model\IngredientTable;
 use UP\Cake\Model\RecipeIngredientTable;
 
 class RecipeService
@@ -17,19 +14,11 @@ class RecipeService
 	public static function getRecipeDetailById(int $id)
 	{
 		$recipe = \UP\Cake\Model\RecipeTable::query()
-										 ->setSelect(['*','USER','INSTRUCTIONS', 'TAGS', 'RECIPE_INGREDIENT.RECIPE_ID'])
+										 ->setSelect(['*','USER','INSTRUCTIONS', 'TAGS', 'RECIPE_INGREDIENT'])
 										 ->where('ID', $id)->fetchObject();
-
-		$recipeId = [];
-		foreach ($recipe->getRecipeIngredient() as $item)
-		{
-			$recipeId[] = $item->getRecipeId();
-		}
+		$ingredientsIds = array_map(function($item) {return $item->getRecipeId();}, $recipe->getRecipeIngredient());
 		$ingredients = RecipeIngredientTable::query()->setSelect(['INGREDIENT.NAME', 'COUNT', 'TYPE_ID'])
-													 ->whereIn('RECIPE_ID', $recipeId)->fetchCollection();
-
-
-		$testArray = [$recipe, $ingredients];
-		return $testArray;
+													 ->whereIn('RECIPE_ID', $ingredientsIds)->fetchCollection();
+		return [$recipe, $ingredients];
 	}
 }
