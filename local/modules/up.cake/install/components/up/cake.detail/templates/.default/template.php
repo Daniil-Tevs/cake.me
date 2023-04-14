e08ae59d62900a1d1045884ae478c6181fbe0773
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-14 17:23:16 +0200

    cake.detail/templates.php: refactor frontend add slider

--- local/modules/up.cake/install/components/up/cake.detail/templates/.default/template.php
+++ local/modules/up.cake/install/components/up/cake.detail/templates/.default/template.php
@@ -13,82 +13,121 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 }
 
 Loc::loadMessages(__FILE__);
-$recipe = $arResult['RECIPE'][0];
-$testIngredients = $arResult['RECIPE'][1];
-// var_dump($recipe->getRecipeIngredient());
+[$recipe, $ingredients] = $arResult['RECIPE'];
+$images = $arResult['IMAGES'];
 ?>
 
-
-	<div class="column">
-		<div class="card detail-card">
-			<div class="card-image">
-				<figure class="image is-4by3">
-					<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-
-				</figure>
-
-			</div>
-			<div class="card-content">
-				<div class="content is-size-6">
-					<div class="title mb-2"><?=htmlspecialcharsbx($recipe->getName())?> </div>
-					<hr>
-					<div class="tags are-large">
-						<?php foreach ($recipe->getTags() as $tag):?>
+<div class="column">
+	<div class="card detail-card">
+		<div class="card-content">
+			<div class="content is-size-6">
+				<div class="title mb-2"><?= htmlspecialcharsbx($recipe->getName()) ?> </div>
+				<hr>
+				<div class="tags are-large">
+					<?php
+					foreach ($recipe->getTags() as $tag): ?>
 						<span class="tag is-danger is-light"><?= htmlspecialcharsbx($tag->getName()) ?></span>
-						<?php endforeach; ?>
-					</div>
-					<p ><?=htmlspecialcharsbx($recipe->getDescription())?></p>
+					<?php
+					endforeach; ?>
 				</div>
+				<p><?= htmlspecialcharsbx($recipe->getDescription()) ?></p>
+			</div>
+		</div>
+
+		<div class="swiper">
+			<div class="swiper-wrapper">
+				<?php
+				foreach ($images as $image): ?>
+					<div class="swiper-slide">
+						<img src="<?= $image ?>" alt="">
+					</div>
+				<?php
+				endforeach; ?>
+				<div class="swiper-pagination"></div>
 			</div>
 
-			<div class="card-content">
-				<div class="media">
-					<div class="media-left">
-						<figure class="image is-48x48">
-							<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-						</figure>
-					</div>
-					<div class="media-content">
-						<p class="title is-4"><a href="#"> <?=htmlspecialcharsbx($recipe->getUser()->getName() . $recipe->getUser()->getLastName())?> </a></p>
-					</div>
+			<div class="swiper-button-next"></div>
+			<div class="swiper-button-prev"></div>
+		</div>
+
+		<div class="card-content">
+			<div class="media">
+				<div class="media-left">
+					<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+					</figure>
+				</div>
+				<div class="media-content">
+					<p class="title is-4"><a href="#"> <?= htmlspecialcharsbx(
+								$recipe->getUser()->getName() . $recipe->getUser()->getLastName()
+							) ?> </a></p>
 				</div>
 			</div>
+		</div>
 
-			<footer class="card-footer">
-				<div class="card-footer-item is-size-6">Время приготовления: <?=htmlspecialcharsbx($recipe->getTime())?> минут</div>
-				<div class="card-footer-item is-size-6">Калории: <?=htmlspecialcharsbx($recipe->getCalories())?></div>
-				<div class="card-footer-item is-size-6">Количество порций: <?=htmlspecialcharsbx($recipe->getPortionCount())?></div>
-			</footer>
-			<hr>
-			<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-				<thead>
+		<footer class="card-footer">
+			<div class="card-footer-item is-size-6">Время приготовления: <?= htmlspecialcharsbx(
+					$recipe->getTime()
+				) ?> минут
+			</div>
+			<div class="card-footer-item is-size-6">Калории: <?= htmlspecialcharsbx($recipe->getCalories()) ?></div>
+			<div class="card-footer-item is-size-6">Количество порций: <?= htmlspecialcharsbx(
+					$recipe->getPortionCount()
+				) ?></div>
+		</footer>
+		<hr>
+		<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+			<thead>
+			<tr>
+				<th></th>
+				<th>Название</th>
+				<th>Количество</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php
+			$countIngredient = 0;
+			foreach ($ingredients as $ingredient): ?>
 				<tr>
-					<th></th>
-					<th>Название</th>
-					<th>Количество</th>
-				</tr>
-				</thead>
-				<tbody>
-				<?php $countIngredient = 0;
-				foreach ($testIngredients as $ingredient): ?>
-				<tr>
-					<th><?= $countIngredient + 1  ?></th>
+					<th><?= $countIngredient + 1 ?></th>
 					<td><?= htmlspecialcharsbx($ingredient->getIngredient()->getName()) ?></td>
-					<td><?= htmlspecialcharsbx($ingredient->getCount()) ?> <?= htmlspecialcharsbx($ingredient->getTypeId()) ?></td>
+					<td><?= htmlspecialcharsbx($ingredient->getCount()) ?> <?= htmlspecialcharsbx(
+							$ingredient->getTypeId()
+						) ?></td>
 				</tr>
-				<?php $countIngredient++; endforeach; ?>
-				</tbody>
-			</table>
+				<?php
+				$countIngredient++; endforeach; ?>
+			</tbody>
+		</table>
 
-			<?php foreach ($recipe->getInstructions() as $instruction):?>
-				<div class="box is-size-6">
-					<div class="content ml-2">
-						<h2>Шаг <?= htmlspecialcharsbx($instruction->getStep())?> </h2>
-					</div>
+		<?php
+		foreach ($recipe->getInstructions() as $instruction): ?>
+			<div class="box is-size-6">
+				<div class="content ml-2">
+					<h2>Шаг <?= htmlspecialcharsbx($instruction->getStep()) ?> </h2>
+				</div>
 				<?= htmlspecialcharsbx($instruction->getDescription()) ?>
 			</div>
-			<?php endforeach; ?>
-		</div>
+		<?php
+		endforeach; ?>
 	</div>
+</div>
 
 </div>
+<script>let swiper = new Swiper('.swiper', {
+		spaceBetween: 5,
+		loop: true,
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		pagination: {
+			el: '.swiper-pagination',
+		},
+
+		mousewheel: false,
+		keyboard: true,
+	});
+</script>
