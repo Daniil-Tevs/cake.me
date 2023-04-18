3a52d41bb4478da9697b77a6006785e9cbf04107
Artyom Pavliuchenya <revinston@gmail.com>
2023-04-18 14:01:01 +0200

    cake.php - добавление роутинга для авторизации, регистрации, выхода пользователя из сети и профиля

--- local/modules/up.cake/install/routes/cake.php
+++ local/modules/up.cake/install/routes/cake.php
@@ -5,9 +5,20 @@ use Bitrix\Main\Routing\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
 
+	//каталог
 	$routes->get('/', new PublicPageController('/local/modules/up.cake/views/cake-list.php'));
 	$routes->get('/catalog/', new PublicPageController('/local/modules/up.cake/views/cake-list.php'));
 	$routes->get('/detail/{id}/', new PublicPageController('/local/modules/up.cake/views/cake-detail.php'));
 	$routes->get('/search/', new PublicPageController('/local/modules/up.cake/views/cake-search.php'));
+
+	//авторизация
+	$routes->get('/register/', new PublicPageController('/local/modules/up.cake/views/cake-register.php'));
+	$routes->get('/auth/', new PublicPageController('/local/modules/up.cake/views/cake-auth.php'));
+	$routes->post('/register/', new PublicPageController('/local/modules/up.cake/views/cake-register.php'));
+	$routes->post('/auth/', new PublicPageController('/local/modules/up.cake/views/cake-auth.php'));
+	$routes->get('/logout/', new PublicPageController('/local/modules/up.cake/views/cake-logout.php'));
+
+	//профиль
+	$routes->get('/profile/', new PublicPageController('/local/modules/up.cake/views/cake-profile.php'));
 
 };
