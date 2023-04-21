82df9d736d0e25a702dd5e68f8a88dcc69e2ba43
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-21 23:10:29 +0200

    remove unused method

--- local/modules/up.cake/install/components/up/cake.list/class.php
+++ local/modules/up.cake/install/components/up/cake.list/class.php
@@ -4,13 +4,6 @@ class CakeListComponent extends CBitrixComponent
 {
 	public function executeComponent()
 	{
-		$this->fetchRecipes();
 		$this->includeComponentTemplate();
-	}
-
-	protected function fetchRecipes(): void
-	{
-		$this->arResult['RECIPES'] = \Up\Cake\Service\RecipeService::get();
-		$this->arResult['IMAGES'] = \Up\Cake\Service\ImageService::get();
 	}
 }
