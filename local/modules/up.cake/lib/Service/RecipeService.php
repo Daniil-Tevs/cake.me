296f04c24598f1104faeac1b496c62d1d7702761
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-21 23:08:03 +0200

    RecipeService.php: add offset and limit in method get

--- local/modules/up.cake/lib/Service/RecipeService.php
+++ local/modules/up.cake/lib/Service/RecipeService.php
@@ -6,35 +6,51 @@ use UP\Cake\Model\RecipeIngredientTable;
 
 class RecipeService
 {
-	public static function get(string $title = null, $filter = null)
+	public static function get(int $offset = 0, int $limit = 0, string $title = null, $filter = null)
 	{
-		$recipes = \UP\Cake\Model\RecipeTable::query()->setSelect(['*','USER','TAGS']);
-		if($title)
+		$recipes = \UP\Cake\Model\RecipeTable::query()->setSelect(
+			[
+				'ID',
+				'NAME',
+				'DESCRIPTION',
+				'TIME',
+				'REACTION',
+				'CALORIES',
+				'PORTION_COUNT',
+				'USER_ID',
+				'DATE_ADDED',
+				'DATE_UPDATED',
+				'USER',
+			]
+		);
+		if ($title)
 		{
 			$title = mySqlHelper()->forSql('%' . $title . '%');
-			$recipes->whereLike('NAME',$title);
+			$recipes->whereLike('NAME', $title);
 		}
-		if($filter)
+		if ($filter)
 		{
 			$filter = mySqlHelper()->convertToDbInteger($filter);
-			$recipes->whereIn('TAGS.ID',$filter);
+			$recipes->whereIn('TAGS.ID', $filter);
 		}
-		return $recipes->fetchCollection();
+		return $recipes->setOffset($offset)->setLimit($limit)->fetchAll();
 	}
 
 	public static function getRecipeDetailById(int $id)
 	{
-		$recipe = \UP\Cake\Model\RecipeTable::query()
-											->setSelect(['*','USER','INSTRUCTIONS', 'TAGS', 'RECIPE_INGREDIENT.RECIPE_ID'])
-											->where('ID', $id)->fetchObject();
+		$recipe = \UP\Cake\Model\RecipeTable::query()->setSelect(
+				['*', 'USER', 'INSTRUCTIONS', 'TAGS', 'RECIPE_INGREDIENT.RECIPE_ID']
+			)->where('ID', $id)->fetchObject();
 
 		$recipeId = [];
 		foreach ($recipe->getRecipeIngredient() as $item)
 		{
 			$recipeId[] = $item->getRecipeId();
 		}
-		$ingredients = RecipeIngredientTable::query()->setSelect(['INGREDIENT.NAME', 'COUNT', 'TYPE_ID'])
-											->whereIn('RECIPE_ID', $recipeId)->fetchCollection();
+		$ingredients = RecipeIngredientTable::query()->setSelect(['INGREDIENT.NAME', 'COUNT', 'TYPE_ID'])->whereIn(
+				'RECIPE_ID',
+				$recipeId
+			)->fetchCollection();
 
 		return [$recipe, $ingredients];
 	}
