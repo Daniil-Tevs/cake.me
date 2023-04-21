4844463193ae2c6e8f6744509b90df59b60c248b
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-21 23:10:09 +0200

    remove unused parameters

--- local/modules/up.cake/views/cake-list.php
+++ local/modules/up.cake/views/cake-list.php
@@ -6,10 +6,6 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Cake.Me");
 
-$APPLICATION->IncludeComponent('up:cake.list', '', [
-	'COUNT_ELEMENTS' => 3,
-	'LENGTH_DESCRIPTION' => 200,
-	'ENCODING' => 'UTF-8',
-]);
+$APPLICATION->IncludeComponent('up:cake.list', '', []);
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");?>
