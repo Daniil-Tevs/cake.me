c622e0ba87400c56f867811beb566dcc0232e3c2
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-21 23:08:29 +0200

    refactor for js extension

--- local/modules/up.cake/install/components/up/cake.list/templates/.default/template.php
+++ local/modules/up.cake/install/components/up/cake.list/templates/.default/template.php
@@ -12,38 +12,53 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 	die();
 }
 
+\Bitrix\Main\UI\Extension::load('up.recipe-list');
+
 Loc::loadMessages(__FILE__);
 $images = $arResult['IMAGES'];
 ?>
 
-<?php
-$i = 0;
-foreach ($arResult['RECIPES'] as $recipe): ?>
-	<?php
-	echo ($i % $arParams['COUNT_ELEMENTS'] === 0) ? '<div class="columns">' : '';
-	$i++; ?>
-	<div class="column mt-5">
-		<div class="card card-list">
-			<div class="card-image">
-				<figure class="image">
-					<img src="<?php echo $images["up.cake_" . $recipe->getId() . "_1"]??'https://bulma.io/images/placeholders/1280x960.png' ?>" alt="Placeholder image">
-				</figure>
-			</div>
-			<div class="card-content">
-				<div class="content">
-					<a class="title mb-2" href="/detail/<?=htmlspecialcharsbx($recipe->getId())?>/"><?=htmlspecialcharsbx($recipe->getName())?> </a>
-					<hr>
-					<p><?=htmlspecialcharsbx(mb_strcut($recipe->getDescription(),0,$arParams['LENGTH_DESCRIPTION'],$arParams['ENCODING']))?></p>
-				</div>
-			</div>
-			<footer class="card-footer">
-				<div class="card-footer-item">🕔 <?=htmlspecialcharsbx($recipe->getTime())?> min</div>
-				<div class="card-footer-item">🔥 135 calories</div>
-				<div class="card-footer-item "><a href="/users/<?=htmlspecialcharsbx($recipe->getUser()->getID())?>">👨‍🍳 <?=htmlspecialcharsbx($recipe->getUser()->getName() . $recipe->getUser()->getLastName())?></a></div>
-			</footer>
-		</div>
-	</div>
-	<?= ($i % 3 === 0) ? '</div>' : ''; ?>
-<?php
-endforeach; ?>
-</div>
+<div id="recipe-list"></div>
+
+<script>
+	let step = 1
+
+	BX.ready(function() {
+		window.CakeRecipeList = new BX.Up.Cake.RecipeList({
+			rootNodeId: 'recipe-list',
+		})
+	})
+
+	function throttle(callee, timeout) {
+		let timer = null
+
+		return function perform(...args) {
+			if (timer) return
+
+			timer = setTimeout(() => {
+				callee(...args)
+
+				clearTimeout(timer)
+				timer = null
+			}, timeout)
+		}
+	}
+
+	async  function checkPosition() {
+		const height = document.body.offsetHeight
+		const screenHeight = window.innerHeight
+		const scrolled = window.scrollY
+
+		const threshold = height - screenHeight / 8
+		const position = scrolled + screenHeight
+
+		if (position >= threshold && !window.CakeRecipeList.END_PAGE) {
+			step++;
+			window.scrollBy(0,-100);
+			await window.CakeRecipeList.reload(step);
+		}
+	}
+
+	window.addEventListener('scroll', throttle(checkPosition))
+
+</script>
