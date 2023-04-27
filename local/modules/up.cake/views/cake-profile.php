a6b1fd3b84636c159d6a2d53aa9e6ddb3d45b5de
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-27 21:43:46 +0200

    cake-profile.php: add include from up.cake.profile

--- local/modules/up.cake/views/cake-profile.php
+++ local/modules/up.cake/views/cake-profile.php
@@ -16,16 +16,6 @@ if (!$USER->IsAuthorized())
 	LocalRedirect("/");
 }
 
-$APPLICATION->IncludeComponent("bitrix:main.profile","", [
-	"USER_PROPERTY_NAME" => "",
-	"SET_TITLE" => "Y",
-	"AJAX_MODE" => "N",
-	"USER_PROPERTY" => Array(),
-	"SEND_INFO" => "Y",
-	"CHECK_RIGHTS" => "Y",
-	"AJAX_OPTION_JUMP" => "N",
-	"AJAX_OPTION_STYLE" => "Y",
-	"AJAX_OPTION_HISTORY" => "N"
-]);
+$APPLICATION->IncludeComponent("up:cake.profile","", []);
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
