99131bcc3cdbbae4337b579de478d18232988897
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-27 14:38:53 +0200

    cake-register.php: add fiels Name,LastName,Personaly_city

--- local/modules/up.cake/views/cake-register.php
+++ local/modules/up.cake/views/cake-register.php
@@ -15,15 +15,20 @@ if ($USER->IsAuthorized())
 $APPLICATION->IncludeComponent('bitrix:main.register', '', [
 	"USER_PROPERTY_NAME" => "",
 	"SEF_MODE" => "N",
-
 	"SHOW_FIELDS" => [
-		0 => "EMAIL",
+		0 => "NAME",
+		1 => "LAST_NAME",
 		2 => "PERSONAL_GENDER",
 		3 => "PERSONAL_NOTES",
+		4 => "PERSONAL_CITY",
+		5 => "EMAIL",
+
 	],
 	"REQUIRED_FIELDS" => [
-		0 => "EMAIL",
+		0 => "NAME",
+		1 => "LAST_NAME",
 		2 => "PERSONAL_GENDER",
+		5 => "EMAIL",
 	],
 	"AUTH" => "N",
 	"USE_BACKURL" => "N",
