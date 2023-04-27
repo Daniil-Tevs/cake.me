7f7ac71fda2716636e718dc0bcaca23b782453a7
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-27 21:56:28 +0200

    Recipe.php: add work for profile

--- local/modules/up.cake/lib/Controller/Recipe.php
+++ local/modules/up.cake/lib/Controller/Recipe.php
@@ -9,17 +9,33 @@ class Recipe extends \Bitrix\Main\Engine\Controller
 {
 	protected const COUNT_RECIPE_ON_PAGE = 4;
 
-	public function getListAction(int $step = 1): array
+	public function getListAction(int $step = 1, int $userId = null): array
 	{
-		$recipeList = array_map(function($recipe) {
-			return array_map(function($data) {
-				return \CUtil::JSEscape($data);
-			}, $recipe);
-		},RecipeService::get(0,$step * self::COUNT_RECIPE_ON_PAGE));
+		if($userId !== null)
+		{
+			$recipeList = array_map(function($recipe) {
+				return array_map(function($data) {
+					return \CUtil::JSEscape($data);
+				}, $recipe);
+			},RecipeService::getRecipeByUserId($userId,0,$step * self::COUNT_RECIPE_ON_PAGE));
+		}
+		else
+		{
+			$recipeList = array_map(function($recipe) {
+				return array_map(function($data) {
+					return \CUtil::JSEscape($data);
+				}, $recipe);
+			},RecipeService::get(0,$step * self::COUNT_RECIPE_ON_PAGE));
+		}
 
-		$imageList = array_map(function($img) {
-			return \CUtil::JSEscape($img);
-		},ImageService::getByIdIn(array_map(fn($recipe) => $recipe['ID'], $recipeList)));
+		$imageList = [];
+		$recipeId = null;
+		foreach (ImageService::getByIdIn(array_map(fn($recipe) => $recipe['ID'], $recipeList)) as $img)
+		{
+			if($recipeId === $img['RECIPE_ID']) continue;
+			$recipeId = $img['RECIPE_ID'];
+			$imageList[$recipeId] = \CUtil::JSEscape(\CFile::GetPath((int)$img['IMAGE_ID']));
+		}
 
 		return [
 			'recipeList' => $recipeList,
