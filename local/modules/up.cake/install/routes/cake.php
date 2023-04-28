b20ad4ec91ec2f20ca44f01b6b92704dc0265a93
Artyom Pavliuchenya <revinston@gmail.com>
2023-04-28 15:07:32 +0200

    cake.php: добавление в роутинг обновление рецепта

--- local/modules/up.cake/install/routes/cake.php
+++ local/modules/up.cake/install/routes/cake.php
@@ -26,4 +26,8 @@ return function (RoutingConfigurator $routes) {
 	//рецепт
 	$routes->get('/recipe/create/', new PublicPageController('/local/modules/up.cake/views/cake-recipe-create.php'));
 	$routes->post('/recipe/create/', new PublicPageController('/local/modules/up.cake/views/cake-recipe-create.php'));
+
+	$routes->get('/recipe/update/{id}/', new PublicPageController('/local/modules/up.cake/views/cake-recipe-update.php'));
+	$routes->post('/recipe/update/{id}/', new PublicPageController('/local/modules/up.cake/views/cake-recipe-update.php'));
+
 };
