6c1b8e42ce11efa67ad84b86c80c11403706f312
Daniil-Tevs <daniil_tevs@mail.ru>
2023-04-30 12:24:34 +0200

    header.php: add filter

--- local/modules/up.cake/install/templates/cake/header.php
+++ local/modules/up.cake/install/templates/cake/header.php
@@ -4,7 +4,8 @@
  * @var array $tags
  * @global CUser $USER
  */
-?><!doctype html>
+?>
+<!doctype html>
 <html lang="<?= LANGUAGE_ID; ?>">
 <head>
 	<meta charset="UTF-8">
@@ -35,20 +36,83 @@ $APPLICATION->ShowPanel(); ?>
 		</div>
 
 		<div class="search-container">
-			<form action="/search/" class="search-form" method="get">
-				<input type="text" name="search-string" class="search-input" placeholder="Search">
+			<form action="/" class="search-form" method="get">
+				<input type="text" name="search-string" class="search-input" placeholder="Search" oninput="window.CakeRecipeList.changeTitle(this.value);window.step=1;">
 				<input type="image" class="search-form-image" name="search-string"  src="/local/modules/up.cake/install/templates/cake/images/search.png" alt="Submit Form" />
-
 			</form>
 		</div>
 
 		<div class="header-two">
-			<a href="/recipe/create/" class="dropdown-item logo-add">
-				➕
+			<!--  adding!-->
+			<a href="/recipe/create/" class="logo-add">
+				<figure class="image is-32x32">
+					<img class="profile-image" aria-controls="dropdown-menu4" src="/local/modules/up.cake/install/templates/cake/images/adding.png">
+				</figure>
 			</a>
+			<!--  filter!-->
+			<div class="dropdown is-right profile-icon" id="category">
+				<div class="dropdown-trigger" >
+					<figure class="image is-32x32">
+						<img class="profile-image " onclick="displayCategory()" aria-controls="dropdown-menu5" src="/local/modules/up.cake/install/templates/cake/images/categories.png">
+					</figure>
+					</div>
+				<div class="dropdown-menu profile header-category" id="dropdown-menu5" role="menu">
+					<div class="dropdown-content">
+						<form>
+							<div class="columns">
+								<?php foreach(\Up\Cake\Service\TagService::getWithCategory() as $category => $tags):?>
+									<div class="column">
+										<div class="dropdown-item">
+											<div class="category-title">
+												<h2><?=htmlspecialcharsbx($category)?>:</h2>
+											</div>
+
+											<div class="tags">
+												<?php foreach ($tags as $tag):?>
+													<div class="tag">
+														<input type="radio" name="<?=htmlspecialcharsbx($category)?>" class="mr-2" oninput="window.CakeRecipeList.changeFilters(this.name,this.value);window.step=1;"   value="<?=htmlspecialcharsbx($tag->getId())?>"><?=htmlspecialcharsbx($tag->getName())?>
+													</div>
+												<?php endforeach;?>
+												<div class="tag"><input type="radio" name="<?=htmlspecialcharsbx($category)?>" class="mr-2" oninput="window.CakeRecipeList.changeFilters(this.name);window.step=1;" checked="true">Всё</div>
+											</div>
+										</div>
+									</div>
+								<?php endforeach;?>
+
+							</div>
+						</form>
+					</div>
+
+
+				</div>
+			</div>
+			<script>
+				document.categoryActive = false;
+				async function displayCategory()
+				{
+
+					if(!document.categoryActive)
+					{
+						document.getElementById('category').classList.add('is-active');
+						document.categoryActive = true;
+					}
+					else{
+						document.getElementById('category').classList.remove('is-active');
+						document.categoryActive = false;
+					}
+				}
+
+				function addFilter()
+				{
+					console.log(1);
+				}
+			</script>
+			<!-- logo-profile !-->
 			<div class="dropdown is-hoverable is-right profile-icon">
 				<div class="dropdown-trigger">
-					<img class="profile-image " aria-controls="dropdown-menu4" src="/local/modules/up.cake/install/templates/cake/images/profile.png">
+					<figure class="image is-48x48">
+						<img class="profile-image " aria-controls="dropdown-menu4" src="/local/modules/up.cake/install/templates/cake/images/profile.png">
+					</figure>
 				</div>
 				<div class="dropdown-menu profile" id="dropdown-menu4" role="menu">
 
@@ -81,12 +145,14 @@ $APPLICATION->ShowPanel(); ?>
 
 <section class="section">
 	<div class="container">
-		<div class="layout tags mt-3">
-			<div class="header header-filter p-1">
+		<div class="layout mt-3">
+			<div class="container filters p-1">
 				<div class="collection-filter">
-					<?php foreach (\Up\Cake\Service\TagService::get() as $tag):?>
-					<a href="/search/?filter=<?=htmlspecialcharsbx($tag->getId())?>" class="header-tag"><strong><?=htmlspecialcharsbx($tag->getName())?></strong></a>
-					<?php endforeach;?>
-				</div>
+					<input class="category-check" type="checkbox" id="popular" name="popular">
+					<label class="label-filter" for="popular"><strong>Популярное</strong></label>
+					<input class="category-check" type="checkbox" id="new-recipes" name="new-recipes">
+					<label class="label-filter" for="new-recipes"><strong>Новинки</strong></label>
+					<input class="category-check" type="checkbox" id="follow" name="follow">
+					<label class="label-filter" for="follow"><strong>Подписки</strong></label>
 			</div>
 		</div>
