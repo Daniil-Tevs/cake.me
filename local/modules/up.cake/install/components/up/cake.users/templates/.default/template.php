b408dbad78384136354a2b5bb893c3c53310decc
Artyom Pavliuchenya <revinston@gmail.com>
2023-05-01 18:02:31 +0200

    Добавление страницы другого пользователя, Возможность подписаться на пользователя и отписаться от него, уведомления об успешности или ошибке при подписке

---
+++ local/modules/up.cake/install/components/up/cake.users/templates/.default/template.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * @var array $arResult
+ * @var array $arParams
+ */
+
+use Bitrix\Main\Localization\Loc;
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+\Bitrix\Main\UI\Extension::load('up.recipe-list');
+
+$user = $arResult['USER'];
+$subCheck = $arResult['SUB_CHECK_SUCCESS'];
+
+Loc::loadMessages(__FILE__);
+?>
+
+<style> .tags {display: none;}</style>
+
+<?php if ($arResult["SUBS_ERROR"] === true): ?>
+	<div class="notification is-danger is-light error-edit-recipe">
+		<p>Вы уже подписаны на этого пользователя!</p>
+	</div>
+<?php endif; ?>
+
+<?php if ($arResult["SUBS_SUCCESS"] === true): ?>
+	<div class="notification is-primary is-light error-edit-recipe">
+		<p>Вы успешно подписались на пользователя!</p>
+	</div>
+<?php elseif ($arResult["SUBS_SUCCESS"] === false): ?>
+	<div class="notification is-danger is-light error-edit-recipe">
+		<p>Не удалось подписаться на пользователя!</p>
+	</div>
+<?php endif; ?>
+
+<?php if ($arResult["DELETE_SUCCESS"] === true): ?>
+	<div class="notification is-primary is-light error-edit-recipe">
+		<p>Вы отписались от пользователя!</p>
+	</div>
+<?php elseif ($arResult["DELETE_SUCCESS"] === false): ?>
+	<div class="notification is-danger is-light error-edit-recipe">
+		<p>Не удалось отписаться от пользователя!</p>
+	</div>
+<?php endif; ?>
+
+<div class="container mt-6">
+	<div class="columns profile">
+		<div class="column profile-img is-two-fifths">
+				<figure class="image card-image is-1by1">
+					<?php if ($user['PERSONAL_PHOTO'] === null): ?>
+					<?php if ($user['PERSONAL_GENDER']==='M'): ?>
+					<img src="/local/modules/up.cake/install/templates/cake/images/profileMale.png" alt="/">
+					<?php else: ?>
+					<img src="/local/modules/up.cake/install/templates/cake/images/profileFemale.png" alt="/">
+					<?php endif; ?>
+					<?php endif; ?>
+					<?= CFile::ShowImage($user['PERSONAL_PHOTO'], 1000, 1000, "border=0", "", true); ?>
+
+				</figure>
+		</div>
+		<div class="column">
+			<div class="card-content">
+				<div class="profile-header">
+					<p class="title is-3 mb-0"><?= htmlspecialcharsbx("{$user['NAME']} {$user['LAST_NAME']}")?></p>
+
+					<?php if ($subCheck === true): ?>
+					<div class="field is-flex">
+						<div class="field is-flex users-subs-icon">
+							<figure class="image is-32x32 is-pulled-right">
+								<img src="/local/modules/up.cake/install/templates/cake/images/subscribeSuccess.png" >
+							</figure>
+							<div class="users-subs-text-check">Вы подписаны</div>
+
+							<a class="button is-danger" href="/users/<?= $user['ID'] ?>/?subsDel=Y">Отписаться</a>
+						</div>
+
+					</div>
+					<?php else: ?>
+						<a href="/users/<?= $user['ID'] ?>/?subs=Y">
+						<div class="field is-flex users-subs-icon">
+							<figure class="image is-32x32 is-pulled-right">
+								<img src="/local/modules/up.cake/install/templates/cake/images/subscribe.png" >
+							</figure>
+							<div class="users-subs-text">Подписаться</div>
+						</div>
+						</a>
+					<?php endif; ?>
+
+				</div>
+
+				<hr class="hr-profile">
+				<div class="content profile-details">
+
+					<div class="profile-detail-info"><p>Пол:</p><?= htmlspecialcharsbx(($user['PERSONAL_GENDER']==='M')?'Мужской':'Женский' ?? 'Не указан')?></div>
+					<div class="profile-detail-info"><p>Город проживания:</p><?= htmlspecialcharsbx($user['PERSONAL_CITY'] ?? 'Не указан')?></div>
+					<div class="profile-detail-info"><p>Описание:</p><?= htmlspecialcharsbx($user['PERSONAL_NOTES'] ?? 'Не указан')?></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div class="your-recipe">
+			<p>Рецепты</p>
+		</div>
+		<div id="recipe-list"></div>
+	</div>
+
+</div>
+
+
+<script>
+	let step = 1;
+	BX.ready(function() {
+		window.CakeRecipeList = new BX.Up.Cake.RecipeList({
+			rootNodeId: 'recipe-list',
+			userId: <?= $user['ID']?>
+		});
+	});
+
+	function throttle(callee, timeout)
+	{
+		let timer = null;
+
+		return function perform(...args) {
+			if (timer)
+			{
+				return;
+			}
+
+			timer = setTimeout(() => {
+				callee(...args);
+
+				clearTimeout(timer);
+				timer = null;
+			}, timeout);
+		};
+	}
+
+	async function checkPosition()
+	{
+		const height = document.body.offsetHeight;
+		const screenHeight = window.innerHeight;
+		const scrolled = window.scrollY;
+
+		const threshold = height - screenHeight / 8;
+		const position = scrolled + screenHeight;
+
+		if (position >= threshold && !window.CakeRecipeList.END_PAGE)
+		{
+			step++;
+			window.scrollBy(0, -100);
+			await window.CakeRecipeList.reload(step);
+		}
+	}
+
+	window.addEventListener('scroll', throttle(checkPosition));
+
+</script>
