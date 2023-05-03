f8afe552887005ab70cd12902cc92689a62f00b5
Artyom Pavliuchenya <revinston@gmail.com>
2023-05-03 15:02:40 +0200

    Нельзя добавить рецепт неавторизованному пользователю

--- local/modules/up.cake/views/cake-recipe-create.php
+++ local/modules/up.cake/views/cake-recipe-create.php
@@ -6,10 +6,10 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Create recipe");
 
-// if (!$USER->IsAuthorized())
-// {
-// 	LocalRedirect("/?authUser=N");
-// }
+if (!$USER->IsAuthorized())
+{
+	LocalRedirect("/?authUser=N");
+}
 
 $APPLICATION->IncludeComponent('up:cake.createRecipe', '', [
 
