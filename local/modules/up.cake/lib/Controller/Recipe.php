8a5db1d62211cc41c55821f43b55da2e94c8401e
Daniil-Tevs <daniil_tevs@mail.ru>
2023-05-04 18:01:39 +0200

    Recipe.php: add work with likes and some refactor

--- local/modules/up.cake/lib/Controller/Recipe.php
+++ local/modules/up.cake/lib/Controller/Recipe.php
@@ -3,6 +3,7 @@
 namespace UP\Cake\Controller;
 
 use Up\Cake\Service\ImageService;
+use Up\Cake\Service\ReactionService;
 use Up\Cake\Service\RecipeService;
 use Up\Cake\Service\UserService;
 
@@ -10,26 +11,26 @@ class Recipe extends \Bitrix\Main\Engine\Controller
 {
 	protected const COUNT_RECIPE_ON_PAGE = 4;
 
-	public function getListAction(int $step = 1, int $userId = null,string $title = '', array $filters = []): array
+	public function getListAction(int $step = 1,string $type = null, int $userId = null, int $anotherUserId = null,string $title = '', array $filters = []): array
 	{
-		if($userId !== null)
+		if($type === 'anotherProfile' && $anotherUserId !== null)
 		{
-			$recipeList = array_map(function($recipe) {
-				return array_map(function($data) {
-					return \CUtil::JSEscape($data);
-				}, $recipe);
-			},RecipeService::getRecipeByUserId($userId,0,$step * self::COUNT_RECIPE_ON_PAGE));
+			$recipes = RecipeService::getRecipeByUserId($anotherUserId,0,$step * self::COUNT_RECIPE_ON_PAGE);
+		}
+		elseif ($type === 'profile' && $userId !== null)
+		{
+			$recipes = RecipeService::getRecipeByUserId($userId,0,$step * self::COUNT_RECIPE_ON_PAGE);
 		}
 		else
 		{
 			$recipes = RecipeService::get(0,$step * self::COUNT_RECIPE_ON_PAGE,$title,$filters);
-
-			$recipeList = array_map(function($recipe) {
-				return array_map(function($data) {
-					return \CUtil::JSEscape($data);
-				}, $recipe);
-			},$recipes);
 		}
+
+		$recipeList = array_map(function($recipe) {
+			return array_map(function($data) {
+				return \CUtil::JSEscape($data);
+			}, $recipe);
+		},$recipes);
 
 		$imageList = [];
 		$recipeId = null;
@@ -40,9 +41,16 @@ class Recipe extends \Bitrix\Main\Engine\Controller
 			$imageList[$recipeId] = \CUtil::JSEscape(\CFile::GetPath((int)$img['IMAGE_ID']));
 		}
 
+		static $userReactions = [];
+		if(empty($userReactions))
+		{
+			$userReactions = array_map(fn($reaction) => (int)\CUtil::JSEscape($reaction['RECIPE_ID']), ReactionService::getByUserId($userId));
+		}
+
 		return [
 			'recipeList' => $recipeList,
 			'imageList' => $imageList,
+			'userReactions' => $userReactions
 		];
 	}
 
