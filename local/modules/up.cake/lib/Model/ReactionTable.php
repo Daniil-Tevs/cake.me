1f518932dc2df9693eea00de33f06f2b6393482f
Daniil-Tevs <daniil_tevs@mail.ru>
2023-05-04 18:06:02 +0200

    ReactionTable.php: tablet for table up_cake_reaction

---
+++ local/modules/up.cake/lib/Model/ReactionTable.php
@@ -0,0 +1,62 @@
+<?php
+namespace UP\Cake\Model;
+
+use Bitrix\Main\Localization\Loc,
+	Bitrix\Main\ORM\Data\DataManager,
+	Bitrix\Main\ORM\Fields\IntegerField;
+use Bitrix\Main\ORM\Fields\Relations\Reference;
+use Bitrix\Main\ORM\Query\Join;
+
+Loc::loadMessages(__FILE__);
+
+/**
+ * Class LikeTable
+ *
+ * Fields:
+ * <ul>
+ * <li> ID int mandatory
+ * <li> USER_ID int mandatory
+ * <li> RECIPE_ID int mandatory
+ * </ul>
+ *
+ * @package Bitrix\Cake
+ **/
+
+class ReactionTable extends DataManager
+{
+	public static function getTableName()
+	{
+		return 'up_cake_reaction';
+	}
+
+	public static function getMap()
+	{
+		return [
+			new IntegerField(
+				'ID',
+				[
+					'primary' => true,
+					'autocomplete' => true,
+				]
+			),
+			new IntegerField(
+				'USER_ID',
+				[
+					'required' => true,
+				]
+			),
+			new IntegerField(
+				'RECIPE_ID',
+				[
+					'required' => true,
+				]
+			),
+			'USER' => (new Reference(
+				'USER', \Bitrix\Main\UserTable::class, Join::on('this.USER_ID', 'ref.ID')
+			))->configureJoinType('inner'),
+			'RECIPE' => (new Reference(
+				'RECIPE', \Bitrix\Main\UserTable::class, Join::on('this.RECIPE_ID', 'ref.ID')
+			))->configureJoinType('inner'),
+		];
+	}
+}
