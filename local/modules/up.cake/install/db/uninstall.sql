3acf0522b7c004788743be28836a9ad23c62b8f6
Artyom Pavliuchenya <revinston@gmail.com>
2023-05-06 00:07:06 +0200

    отредактировал тестовые данные

--- local/modules/up.cake/install/db/uninstall.sql
+++ local/modules/up.cake/install/db/uninstall.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS up_cake_recipe_tag;
-DROP TABLE IF EXISTS up_cake_category;
 DROP TABLE IF EXISTS up_cake_recipe_ingredient;
 DROP TABLE IF EXISTS up_cake_recipe_image;
 DROP TABLE IF EXISTS up_cake_recipe_comment;
@@ -8,6 +7,8 @@ DROP TABLE IF EXISTS up_cake_tag;
 DROP TABLE IF EXISTS up_cake_ingredient;
 DROP TABLE IF EXISTS up_cake_type;
 DROP TABLE IF EXISTS up_cake_instructions;
-DROP TABLE IF EXISTS up_cake_recipe;
+
 DROP TABLE IF EXISTS up_cake_comment;
 DROP TABLE IF EXISTS up_cake_reaction;
+DROP TABLE IF EXISTS up_cake_category;
+DROP TABLE IF EXISTS up_cake_recipe;
