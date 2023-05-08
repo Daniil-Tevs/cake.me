9c7c5bdff0e5b231ba18fc9389442113d95231c9
Artyom Pavliuchenya <revinston@gmail.com>
2023-05-08 21:55:21 +0200

    подписки: js файлы расширения

--- local/modules/up.cake/install/js/up/subscribe-list/dist/subscribe-list.bundle.js
+++ local/modules/up.cake/install/js/up/subscribe-list/dist/subscribe-list.bundle.js
@@ -3,15 +3,91 @@ this.BX.Up = this.BX.Up || {};
 (function (exports,main_core) {
 	'use strict';
 
+	var _templateObject;
 	var SubscribeList = /*#__PURE__*/function () {
 	  function SubscribeList() {
-	    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {
-	      name: 'SubscribeList'
-	    };
+	    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 	    babelHelpers.classCallCheck(this, SubscribeList);
-	    this.name = options.name;
+	    babelHelpers.defineProperty(this, "COUNT_COMMENT_IN_ROW", 10);
+	    babelHelpers.defineProperty(this, "LENGTH_COMMENT", 1024);
+	    babelHelpers.defineProperty(this, "END_PAGE", false);
+	    if (main_core.Type.isStringFilled(options.rootNodeId)) {
+	      this.rootNodeId = options.rootNodeId;
+	    } else {
+	      throw new Error('RecipeList: options.rootNodeId required');
+	    }
+	    if (main_core.Type.isInteger(options.userId)) {
+	      this.userId = options.userId;
+	    }
+	    if (main_core.Type.isInteger(options.subs2)) {
+	      this.subs2 = options.subs2;
+	    }
+	    this.rootNode = document.getElementById(this.rootNodeId);
+	    this.userList = [];
+	    if (!this.rootNode) {
+	      throw new Error("userList: element with id\"".concat(this.rootNodeId, "\" not found"));
+	    }
+	    this.reload();
 	  }
 	  babelHelpers.createClass(SubscribeList, [{
+	    key: "reload",
+	    value: function reload() {
+	      var _this = this;
+	      var step = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+	      this.loadList(step).then(function (userList) {
+	        if (userList.length !== _this.userList.length || userList.length === 0) {
+	          _this.userList = userList;
+	          _this.render();
+	        } else {
+	          _this.END_PAGE = true;
+	        }
+	      });
+	    }
+	  }, {
+	    key: "loadList",
+	    value: function loadList() {
+	      var _this2 = this;
+	      var step = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+	      return new Promise(function (resolve, reject) {
+	        var _this2$userId, _this2$subs;
+	        BX.ajax.runAction('up:cake.subs.getList', {
+	          data: {
+	            step: step,
+	            userId: (_this2$userId = _this2.userId) !== null && _this2$userId !== void 0 ? _this2$userId : null,
+	            subs2: (_this2$subs = _this2.subs2) !== null && _this2$subs !== void 0 ? _this2$subs : null
+	          }
+	        }).then(function (response) {
+	          resolve(response.data.userList);
+	        })["catch"](function (error) {
+	          reject(error);
+	        });
+	      });
+	    }
+	  }, {
+	    key: "render",
+	    value: function render() {
+	      var _this3 = this;
+	      this.rootNode.innerHTML = '';
+	      console.log(this.userList);
+	      if (this.userList.length === 0) {
+	        var commentNode = document.createElement('div'); // is a node
+	        commentNode.innerHTML = "\n\t\t\t\t<br>\n\t\t\t\t<h2>\u041D\u0435\u0442 \u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u0435\u0439!</h2>\n\t\t\t\t";
+	        this.rootNode.appendChild(commentNode);
+	        return;
+	      }
+	      this.userList.forEach(function (commentData) {
+	        if (commentData.PERSONAL_PHOTO === '') {
+	          if (commentData.PERSONAL_GENDER === 'M') {
+	            commentData.PERSONAL_PHOTO = "/local/modules/up.cake/install/templates/cake/images/profileMale.png";
+	          } else {
+	            commentData.PERSONAL_PHOTO = "/local/modules/up.cake/install/templates/cake/images/profileFemale.png";
+	          }
+	        }
+	        var commentNode = main_core.Tag.render(_templateObject || (_templateObject = babelHelpers.taggedTemplateLiteral(["<div class=\"box box-user-search\">\n\t\t\t\t<article class=\"media\">\n\t\t\t\t\t<div class=\"media-left\">\n\t\t\t\t\t\t<a href=\"/users/", "/\">\n<!--\t\t\t\t\t\t\t<figure class=\"image is-64x64\">-->\n\t\t\t\t\t\t\t\t<img src=\"", "\" height=\"150\" width=\"150\" alt=\"image\">\n<!--\t\t\t\t\t\t\t</figure>-->\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t\t\t\n\t\t\t\t\t<div class=\"media-content\">\n\t\t\t\t\t\t<div class=\"content\">\n\t\t\t\t\t\t\t<p>\n\t\t\t\t\t\t\t<a href=\"/users/", "/\">\n\t\t\t\t\t\t\t\t<strong>", "</strong>\n\t\t\t\t\t\t\t</a>\n\t\t\t\t\t\t\t<br>\n\t\t\t\t\t\t\t\t", "\n\t\t\t\t\t\t\t</p>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t</article>\n\t\t\t</div>"])), commentData.ID, commentData.PERSONAL_PHOTO, commentData.ID, commentData.NAME + ' ' + commentData.LAST_NAME + ' (' + commentData.LOGIN + ')', commentData.PERSONAL_NOTES.substring(0, _this3.LENGTH_COMMENT));
+	        _this3.rootNode.appendChild(commentNode);
+	      });
+	    }
+	  }, {
 	    key: "setName",
 	    value: function setName(name) {
 	      if (main_core.Type.isString(name)) {
--- local/modules/up.cake/install/js/up/subscribe-list/src/subscribe-list.js
+++ local/modules/up.cake/install/js/up/subscribe-list/src/subscribe-list.js
@@ -1,10 +1,130 @@
-import {Type} from 'main.core';
+import {Type, Tag} from 'main.core';
 
 export class SubscribeList
 {
-	constructor(options = {name: 'SubscribeList'})
+	COUNT_COMMENT_IN_ROW = 10;
+	LENGTH_COMMENT = 1024;
+	END_PAGE = false;
+
+	constructor(options = {})
 	{
-		this.name = options.name;
+		if (Type.isStringFilled(options.rootNodeId))
+		{
+			this.rootNodeId = options.rootNodeId;
+		}
+		else
+		{
+			throw new Error('RecipeList: options.rootNodeId required');
+		}
+
+		if (Type.isInteger(options.userId))
+		{
+			this.userId = options.userId;
+		}
+
+		if (Type.isInteger(options.subs2))
+		{
+			this.subs2 = options.subs2;
+		}
+
+		this.rootNode = document.getElementById(this.rootNodeId);
+		this.userList = []
+
+		if (!this.rootNode)
+		{
+			throw new Error(`userList: element with id"${this.rootNodeId}" not found`);
+		}
+
+		this.reload();
+	}
+
+	reload(step = 1)
+	{
+		this.loadList(step)
+			.then((userList) => {
+				if (userList.length !== this.userList.length || userList.length === 0)
+				{
+					this.userList = userList;
+					this.render();
+				}
+				else
+				{
+					this.END_PAGE = true;
+				}
+			});
+	}
+
+	loadList(step = 1)
+	{
+		return new Promise((resolve, reject) => {
+			BX.ajax.runAction('up:cake.subs.getList', {
+					data: {
+						step: step,
+						userId: this.userId ?? null,
+						subs2: this.subs2 ?? null,
+					},
+				})
+				.then((response) => {
+					resolve(response.data.userList);
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
+	}
+
+	render()
+	{
+		this.rootNode.innerHTML = '';
+		console.log(this.userList);
+		if (this.userList.length === 0)
+		{
+			var commentNode = document.createElement('div'); // is a node
+			commentNode.innerHTML = `
+				<br>
+				<h2>Нет пользователей!</h2>
+				`;
+			this.rootNode.appendChild(commentNode);
+			return;
+		}
+			this.userList.forEach(commentData => {
+				if (commentData.PERSONAL_PHOTO === '')
+				{
+					if (commentData.PERSONAL_GENDER === 'M')
+					{
+						commentData.PERSONAL_PHOTO = "/local/modules/up.cake/install/templates/cake/images/profileMale.png"
+					}
+					else
+					{
+						commentData.PERSONAL_PHOTO = "/local/modules/up.cake/install/templates/cake/images/profileFemale.png"
+					}
+				}
+
+				const commentNode = Tag.render`<div class="box box-user-search">
+				<article class="media">
+					<div class="media-left">
+						<a href="/users/${commentData.ID}/">
+<!--							<figure class="image is-64x64">-->
+								<img src="${commentData.PERSONAL_PHOTO}" height="150" width="150" alt="image">
+<!--							</figure>-->
+						</a>
+					</div>
+					
+					<div class="media-content">
+						<div class="content">
+							<p>
+							<a href="/users/${commentData.ID}/">
+								<strong>${commentData.NAME + ' ' + commentData.LAST_NAME + ' (' + commentData.LOGIN + ')'}</strong>
+							</a>
+							<br>
+								${commentData.PERSONAL_NOTES.substring(0,this.LENGTH_COMMENT)}
+							</p>
+						</div>
+					</div>
+				</article>
+			</div>`;
+				this.rootNode.appendChild(commentNode);
+			});
 	}
 
 	setName(name)
