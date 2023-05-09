c8a1189cffd6b89530a7b7c497493ea523ff2328
Daniil-Tevs <daniil_tevs@mail.ru>
2023-05-09 02:27:30 +0200

    UserSubsTable.php: добавил поле DATE_ADDED

--- local/modules/up.cake/lib/Model/UserSubsTable.php
+++ local/modules/up.cake/lib/Model/UserSubsTable.php
@@ -4,10 +4,11 @@ namespace UP\Cake\Model;
 use Bitrix\Main\Localization\Loc,
 	Bitrix\Main\ORM\Data\DataManager,
 	Bitrix\Main\ORM\Fields\IntegerField;
+use Bitrix\Main\ORM\Fields\DatetimeField;
 use Bitrix\Main\ORM\Fields\Relations\OneToMany;
 use Bitrix\Main\ORM\Fields\Relations\Reference;
 use Bitrix\Main\ORM\Query\Join;
-
+use Bitrix\Main\Type\DateTime;
 
 Loc::loadMessages(__FILE__);
 
@@ -33,7 +34,14 @@ class UserSubsTable extends DataManager
 					'primary' => true,
 				]
 			),
-
+			new DatetimeField(
+				'DATE_ADDED',
+				[
+					'required' => true,
+					'default_value' => function() {
+						return new DateTime();
+					}
+				]),
 			'USER' => (new Reference(
 				'USER',
 				UserTable::class,
