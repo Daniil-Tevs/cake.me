0a353c1a2ec1454087a4df41f9184bb979562577
Daniil-Tevs <daniil_tevs@mail.ru>
2023-05-11 00:34:53 +0200

    ReactionService.php: добавил метод проверки поставил ли лайк пользователб на этот рецепт

--- local/modules/up.cake/lib/Service/ReactionService.php
+++ local/modules/up.cake/lib/Service/ReactionService.php
@@ -12,6 +12,13 @@ class ReactionService
 		return ReactionTable::query()->setSelect(['RECIPE_ID'])->where('USER_ID', mySqlHelper()->convertToDbInteger($userId))->fetchAll();
 	}
 
+	public static function chekUserReactionByRecipeId(int $userId,int $recipeId):bool
+	{
+		$result = ReactionTable::query()->setSelect(['*'])->where('USER_ID', mySqlHelper()->convertToDbInteger($userId))->where('RECIPE_ID', mySqlHelper()->convertToDbInteger($recipeId))->fetchAll();
+		return !empty($result);
+
+	}
+
 	public static function addLike(int $userId, int $recipeId): bool
 	{
 		$recipeId = (int)mySqlHelper()->convertToDbInteger($recipeId);
