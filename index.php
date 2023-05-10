ec5a7b2a91aefbe6f430f7cfb106ce62f2c1dd50
Daniil-Tevs <daniil_tevs@mail.ru>
2023-05-11 00:30:02 +0200

    index.php: почистил

--- index.php
+++ index.php
@@ -3,13 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 require_once __DIR__ . '/bitrix/routing_index.php';
 
-/**
- * TODO:
-
- * 7) добавить чистку bfile, buser, папки uploads при удалении модуляя
-
- * 8) безопасность и локализация
- */
 ?>
 
 
