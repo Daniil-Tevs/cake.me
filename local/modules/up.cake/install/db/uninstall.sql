048f2d6ffa2f81c62a7d0904792d3b59439df8c2
Daniil-Tevs <daniil_tevs@mail.ru>
2023-05-11 15:30:00 +0200

    Поправил базу данных

--- local/modules/up.cake/install/db/uninstall.sql
+++ local/modules/up.cake/install/db/uninstall.sql
@@ -1,13 +1,11 @@
 DROP TABLE IF EXISTS up_cake_recipe_tag;
 DROP TABLE IF EXISTS up_cake_recipe_ingredient;
 DROP TABLE IF EXISTS up_cake_recipe_image;
-DROP TABLE IF EXISTS up_cake_recipe_comment;
 DROP TABLE IF EXISTS up_cake_user_subs;
 DROP TABLE IF EXISTS up_cake_tag;
 DROP TABLE IF EXISTS up_cake_ingredient;
 DROP TABLE IF EXISTS up_cake_type;
 DROP TABLE IF EXISTS up_cake_instructions;
-
 DROP TABLE IF EXISTS up_cake_comment;
 DROP TABLE IF EXISTS up_cake_reaction;
 DROP TABLE IF EXISTS up_cake_category;
